0a212ce0f9b0f2f389b8c44f926a9807af475117
Lyubomir Dimitrov <lyubomir.v.dimitrov@gmail.com>
2017-10-18 00:27:27 +0300

    initial commit

---
+++ image_retriever.py
@@ -0,0 +1,37 @@
+from urllib import request
+import glob, os
+
+folder_with_urls = 'url_files_test/'
+folder_to_download_images = 'datasets_test/'
+
+attempted = 0
+success = 0
+failed = 0
+for file in glob.glob('*.txt'):
+	array = []
+	if file.endswith('.txt'):
+		with open(file) as fileToRead:
+			array = fileToRead.readlines()
+		fileToRead.closed
+
+	array = [x.strip() for x in array]
+	lastSlash = file.rfind('/') + 1
+	dot = file.rfind('.txt')
+
+	filename = file[lastSlash:dot]
+	folder = folder_to_download_images+ filename
+	if not os.path.exists(folder):
+		os.makedirs(folder)
+
+	for element in array:
+		index = element.rfind('/') + 1
+		try:
+			attempted += 1
+			if 'flickr' in element:
+				success += 1
+				request.urlretrieve(element, 'datasets_test/' + filename + '/' + element[index:])
+		except:
+			failed += 1
+
+print('tried to download ' + str(attempted) + ' images')
+print('finished with ' + str(success) + ' new images and, ' + str(failed) + ' download errors')
