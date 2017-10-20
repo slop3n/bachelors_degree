419da2352bc849e64609ba5f03cebe97b58cfa56
Lyubomir Dimitrov <lyubomir.v.dimitrov@gmail.com>
2017-10-20 17:48:55 +0300

    modify the image retreiver and shorten the test files

--- image_retriever.py
+++ image_retriever.py
@@ -1,38 +1,56 @@
 from urllib import request
 import glob, os
+import argparse
 
-folder_with_urls = 'url_files/'
-target_folder = 'datasets/'
+def download_files(source, target):
+	attempted = 0
+	success = 0
+	failed = 0
+	for file in glob.glob(source + '*.txt'):
+		array = []
+		if file.endswith('.txt'):
+			with open(file) as fileToRead:
+				array = fileToRead.readlines()
+			fileToRead.closed
 
-attempted = 0
-success = 0
-failed = 0
-for file in glob.glob(folder_with_urls + '*.txt'):
-	array = []
-	if file.endswith('.txt'):
-		with open(file) as fileToRead:
-			array = fileToRead.readlines()
-		fileToRead.closed
+		array = [x.strip() for x in array]
+		lastSlash = file.rfind('/') + 1
+		dot = file.rfind('.txt')
 
-	array = [x.strip() for x in array]
-	lastSlash = file.rfind('/') + 1
-	dot = file.rfind('.txt')
+		filename = file[lastSlash:dot]
+		folder = target + filename
 
-	filename = file[lastSlash:dot]
-	folder = target_folder + filename
-	if not os.path.exists(folder):
-		os.makedirs(folder)
+		if not os.path.exists(folder):
+			os.makedirs(folder)
 
-	for element in array:
-		index = element.rfind('/') + 1
-		try:
-			attempted += 1
-			# most of the images not hosted on flickr are not accessible
-			if 'flickr' in element:
-				success += 1
-				request.urlretrieve(element, target_folder + filename + '/' + element[index:])
-		except:
-			failed += 1
+		for element in array:
+			index = element.rfind('/') + 1
+			try:
+				attempted += 1
+				# most of the images not hosted on flickr are not accessible
+				if 'flickr' in element:
+					success += 1
+					request.urlretrieve(element, target + filename + '/' + element[index:])
+			except:
+				failed += 1
 
-print('tried to download ' + str(attempted) + ' images')
-print('finished with ' + str(success) + ' new images and, ' + str(failed) + ' download errors')
+	print('tried to download ' + str(attempted) + ' images')
+	print('finished with ' + str(success) + ' new images and, ' + str(failed) + ' download errors')
+
+if __name__ == "__main__":
+
+	# some default values in case no arguments are given
+	source = "url_files_test/"
+	target = "datasets_test/"
+
+	parser = argparse.ArgumentParser()
+	parser.add_argument("--source", help="the email of the receiver")
+	parser.add_argument("--target", help="email subject")
+	args = parser.parse_args()
+
+	if args.source:
+		source = args.source
+	if args.target:
+		target = args.target
+
+	download_files(source, target)
