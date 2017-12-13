17982962e95075e055acd656c935a331f26ac78d
Lyubomir Dimitrov <lyubomir.v.dimitrov@gmail.com>
2017-12-13 12:34:51 +0200

    add allowed host

--- web/settings.py
+++ web/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '^8do)ybrj+qcmg!72x@vwh4dsjh9q6cb$2z&ljn(x6txx1odz@'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
