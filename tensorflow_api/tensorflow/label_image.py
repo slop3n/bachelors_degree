fe1115a4c00e7408d583a6d23aa81678a34a57ce
Lyubomir Dimitrov <lyubomir.v.dimitrov@gmail.com>
2018-01-17 13:23:09 +0200

    fix tensorflow folder route

--- tensorflow_api/tensorflow/label_image.py
+++ tensorflow_api/tensorflow/label_image.py
@@ -23,9 +23,9 @@ import os
 
 import numpy as np
 import tensorflow as tf
-# django_dir = "tensorflow_api/tensorflow/"
-django_dir  = ''
-file_name = "3.jpg"
+django_dir = "tensorflow_api/tensorflow/"
+# django_dir  = ''
+file_name = ""
 model_file = django_dir + "tf_files/retrained_graph.pb"
 label_file = django_dir + "tf_files/retrained_labels.txt"
 input_height = 224
