82af72cb975c48869de5d02a3a429df6c19c212b
Lyubomir Dimitrov <lyubomir.v.dimitrov@gmail.com>
2018-01-22 21:46:07 +0200

    fix date template and remove unused urls

--- tensorflow_api/urls.py
+++ tensorflow_api/urls.py
@@ -5,8 +5,6 @@ from . import views
 app_name = 'tensorflow_api'
 urlpatterns=[
 	url(r'^$', views.index, name='index'),
-    url(r'^(?P<item_id>[0-9]+)/$', views.detail, name='detail'),
 	url(r'^scan/$', views.scan, name='scan'),
 	url(r'^items/$', views.items, name='items'),
-	url(r'^chart/$', views.chart, name='chart')
 ]
